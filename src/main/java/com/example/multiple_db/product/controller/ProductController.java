220aded95b9e0475a2fd6c14a8f69a3b820bb847
PRA123IP <pradipgiri644@gmail.com>
2024-09-08 09:42:43 +0530

    add getdetails

--- src/main/java/com/example/multiple_db/product/controller/ProductController.java
+++ src/main/java/com/example/multiple_db/product/controller/ProductController.java
@@ -7,10 +7,9 @@ import com.example.multiple_db.product.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("product/api")
@@ -24,6 +23,16 @@ public class ProductController {
     {
         Product savedProduct = productRepo.save(product);
         return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<List<Product>> getdetails()
+    {
+        final List<Product> productList = productRepo.findAll();
+
+        return new ResponseEntity<>(productList,HttpStatus.OK);
+
+
     }
 
 }
